408b59ac421ab8eb5fec09d458db6f2908977d5a
Charley Shattuck <cshattuck@surewest.net>
2021-09-02 22:21:54 -0700

    1+ and dup for more testing

--- ITC.ino
+++ ITC.ino
@@ -1,15 +1,19 @@
 // ITC.ino
 
 const int PROGMEM memory[] = {
-  5 , // reset  address 0
+  7 , // reset  address 0
   3 , // lit    address 1
   2 , // branch address 2
   0 , // emit   address 3 
   1 , // ms     address 4
+  4 , // dup    address 5
+  5 , // 1+     address 6
   1 , 65 ,   // lit 'A' to stack
+  6 ,        // 1+
+  5 ,        // dup
   3 ,        // emit
   1 , 1000 , // lit 1000 to stack
   4 ,        // ms
-  2 , 5 ,    // branch to address 5
+  2 , 8 ,    // branch to address 8
 };  // begin 65 emit 1000 ms again
 
--- ITC_more.ino
+++ ITC_more.ino
@@ -5,15 +5,19 @@
 // it seems that Serial.h is loaded by default
 
 // circular stack, similar to GA144
-const int STKSIZE = 16;
-const int STKMASK = 15;
+const int STKSIZE = 8;
+const int STKMASK = 7;
 int stack [STKSIZE];
+#define DROP T=stack[S++];S&=STKMASK
+#define PUSH S=--S;S&=STKMASK
+#define DUP PUSH;stack[S]=T
 
 // Forth registers
 int W=0; // working register
 int I=0; // instruction pointer
 int S=0; // data stack pointer
 int R=0; // return stack pointer
+int T=0; // top of stack, cached
 
 void setup() {
   Serial.begin (9600);
@@ -26,10 +30,12 @@ void loop () {
 next: 
   W=pgm_read_word(&memory[I++]);
   switch (pgm_read_word(&memory[W])) {
-    case 0: Serial.write(stack[S++]);goto next; // emit
-    case 1: delay(stack[S++]);goto next;        // ms
+    case 0: Serial.write(T);DROP;goto next; // emit
+    case 1: delay(T);DROP;goto next;        // ms
     case 2: I=pgm_read_word(&memory[I++]);goto next; // branch
-    case 3: stack[--S]=pgm_read_word(&memory[I++]);goto next; // lit
+    case 3: PUSH;T=pgm_read_word(&memory[I++]);goto next; // lit
+    case 4: DUP;goto next; // dup
+    case 5: T=T+1;goto next; // 1+
     default: goto next;
   }  
 }
